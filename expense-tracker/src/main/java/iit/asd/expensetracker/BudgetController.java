8d518e78462fc599c2304d3816adddd04da146de
DinShaCha <shadu.chan@gmail.com>
2024-01-19 01:18:06 +0530

    Commit

---
+++ expense-tracker/src/main/java/iit/asd/expensetracker/BudgetController.java
@@ -0,0 +1,356 @@
+package iit.asd.expensetracker;
+
+import iit.asd.expensetracker.entity.*;
+import iit.asd.expensetracker.service.BudgetService;
+import iit.asd.expensetracker.service.CategoryService;
+import iit.asd.expensetracker.service.TransactionService;
+import iit.asd.expensetracker.service.impl.BudgetServiceImpl;
+import iit.asd.expensetracker.service.impl.CategoryServiceImpl;
+import iit.asd.expensetracker.service.impl.TransactionServiceImpl;
+import iit.asd.expensetracker.util.enums.Month;
+import iit.asd.expensetracker.util.singleton.DataStore;
+import iit.asd.expensetracker.util.singleton.MainStage;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
+
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class BudgetController implements Initializable {
+    private Budget selectedBudget;
+    private boolean isUpdate = false;
+    @FXML
+    private ComboBox<Category> budgetCategory;
+    @FXML
+    private ComboBox<Month> month;
+    @FXML
+    private TextField amountText;
+    @FXML
+    private Label overallProgress;
+    @FXML
+    private ComboBox<Integer> year;
+    @FXML
+    private ComboBox<Integer> yearBudgetProgress;
+    @FXML
+    private ComboBox<Month> monthBudgetProgress;
+    @FXML
+    private TableView<Budget> budgetTable;
+    @FXML
+    private TableView<BudgetUpdate> budgetReportTable;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnDelete;
+    private BudgetService budgetDAO;
+    private TransactionService transactionDAO;
+    private CategoryService categoryDAO;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+
+    public BudgetController() {
+        budgetDAO = new BudgetServiceImpl();
+        transactionDAO = new TransactionServiceImpl();
+        categoryDAO = new CategoryServiceImpl();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnDelete.setVisible(false);
+        initForm();
+        initTableColumns();
+        initTableData();
+        initBudgetProgress();
+    }
+
+    private void initForm() {
+        budgetCategory.setItems(FXCollections.observableArrayList(DataStore.getInstance().getCategoryList()));
+        budgetCategory.setCellFactory(new Callback<ListView<Category>, ListCell<Category>>() {
+            @Override
+            public ListCell<Category> call(ListView<Category> param) {
+                return new ListCell<Category>() {
+                    @Override
+                    protected void updateItem(Category item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setText("");
+                        } else {
+                            setText(item.getName());
+                        }
+                    }
+                };
+            }
+        });
+
+        budgetCategory.setConverter(new StringConverter<Category>() {
+            @Override
+            public String toString(Category category) {
+                if (category == null) {
+                    return null;
+                } else {
+                    return category.getName();
+                }
+            }
+
+            @Override
+            public Category fromString(String categoryString) {
+                return budgetCategory.getItems().stream().filter(a -> a.getName().equals(categoryString)).findFirst().orElse(null);
+            }
+        });
+
+        month.getItems().addAll(Month.JANUARY, Month.FEBRUARY, Month.MARCH, Month.APRIL, Month.MAY, Month.JUNE, Month.JULY, Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
+        month.getSelectionModel().select(Month.JANUARY);
+
+        year.getItems().addAll(2021, 2022, 2023, 2024);
+        year.setValue(2023);
+
+        monthBudgetProgress.getItems().addAll(Month.JANUARY, Month.FEBRUARY, Month.MARCH, Month.APRIL, Month.MAY, Month.JUNE, Month.JULY, Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
+        monthBudgetProgress.getSelectionModel().select(Month.JANUARY);
+
+        yearBudgetProgress.getItems().addAll(2021, 2022, 2023, 2024);
+        yearBudgetProgress.setValue(2023);
+    }
+
+    private boolean isValidAmount() {
+
+        return amountText.getText() != null &&
+                !Objects.equals(amountText.getText(), "") &&
+                budgetCategory.getValue() != null;
+    }
+
+    private boolean isValidCreate(){
+       Budget budget = budgetDAO.getBudgetByMonthOfYearAndCategory(month.getValue(), year.getValue(),budgetCategory.getValue());
+
+        return budget == null;
+    }
+
+    private boolean isValid(int id) {
+        return budgetDAO.getBudgetById(id) != null;
+    }
+
+    @FXML
+    protected void onSaveBudget() {
+        try{
+            if (!isUpdate) {
+                if(isValidAmount() && isValidCreate()) {
+                    Budget budget = new Budget(Float.parseFloat(amountText.getText()), budgetCategory.getValue(), month.getValue(), year.getValue());
+                    create(budget);
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Budget");
+                    alert.setContentText("Please enter valid budget");
+                    alert.showAndWait();
+                }
+
+            } else {
+                if(isValidAmount()) {
+                    selectedBudget = budgetTable.getSelectionModel().getSelectedItem();
+                    selectedBudget.setBudget(Float.parseFloat(amountText.getText()));
+                    selectedBudget.setCategory(budgetCategory.getValue());
+                    selectedBudget.setMonth(month.getValue());
+                    selectedBudget.setYear(year.getValue());
+
+                   this.update(selectedBudget);
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Budget");
+                    alert.setContentText("Please enter valid budget");
+                    alert.showAndWait();
+                }
+            }
+        } catch (NumberFormatException ne) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Budget");
+            alert.setContentText("Please enter valid budget");
+            alert.showAndWait();
+        }
+
+        clearForm();
+        initTableData();
+    }
+
+    private  void create(Budget budget){
+        budgetDAO.create(budget);
+    }
+
+    private void update(Budget budget) {
+        budgetDAO.update(budget);
+    }
+
+    private void delete(int id) {
+        budgetDAO.delete(id);
+    }
+
+    @FXML
+    protected void onDeleteBudget(){
+        if(isValid(selectedBudget.getId())) {
+            this.delete(selectedBudget.getId());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid budget");
+            alert.setContentText("Please select a valid budget to proceed");
+            alert.showAndWait();
+        }
+
+        initTableData();
+        clearForm();
+    }
+
+    @FXML
+    protected void onClickSingleRow() {
+        selectedBudget = budgetTable.getSelectionModel().getSelectedItem();
+
+        Budget selectedTBudget  = budgetTable.getSelectionModel().getSelectedItem();
+        amountText.setText(String.valueOf(selectedTBudget.getBudget()));
+
+        month.setValue(selectedTBudget.getMonth());
+        year.setValue(selectedTBudget.getYear());
+        budgetCategory.setValue(selectedTBudget.getCategory());
+
+        btnSave.setText("Update");
+        btnDelete.setVisible(true);
+        isUpdate = true;
+    }
+
+    private void initTableColumns() {
+        budgetTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<Budget, Integer> columnId = new TableColumn<>("ID");
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Budget, String> columnCategory = new TableColumn<>("Category");
+        columnCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+        TableColumn<Budget, Month> columnMonth = new TableColumn<>("Month");
+        columnMonth.setCellValueFactory(new PropertyValueFactory<>("month"));
+
+        TableColumn<Budget, Integer> columnYear = new TableColumn<>("Year");
+        columnYear.setCellValueFactory(new PropertyValueFactory<>("year"));
+
+        TableColumn<Budget, Double> columnBudget = new TableColumn<>("Budget");
+        columnBudget.setCellValueFactory(new PropertyValueFactory<>("budget"));
+
+        budgetTable.getColumns().addAll(columnId,columnCategory, columnBudget, columnMonth, columnYear);
+    }
+
+    private void initBudgetProgress(){
+        budgetReportTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<BudgetUpdate, String> category = new TableColumn<>("Category");
+        category.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+        TableColumn<BudgetUpdate, String> columnCategory = new TableColumn<>("Progress");
+        columnCategory.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        budgetReportTable.getColumns().addAll(category,columnCategory);
+    }
+
+    private void initTableData() {
+        budgetTable.setItems(FXCollections.observableArrayList(budgetDAO.getAll()));
+        budgetTable.refresh();
+    }
+
+    @FXML
+    protected void onClickBackBtnBudget() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/home-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+            Stage stage = MainStage.getInstance();
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void clearBudgetForm(){
+        clearForm();
+    }
+
+    private void clearForm() {
+        amountText.clear();
+        month.getItems().clear();
+        year.getItems().clear();
+        budgetCategory.getItems().clear();
+
+        initForm();
+        btnSave.setText("Save");
+        btnDelete.setVisible(false);
+        isUpdate = false;
+    }
+
+    private double calculatePercentage(double totalBudget, double totalExpenditure) {
+        return (totalBudget != 0 && totalExpenditure != 0) ? (totalExpenditure / totalBudget) * 100 : 0;
+    }
+
+    private List<BudgetUpdate> calculateCategorizedProgress(List<Category> categories, List<AccountTransaction> transactions, Month m, int y){
+        List<BudgetUpdate> report = new ArrayList<>();
+        for(Category c : categories){
+            float amount = 0;
+            for(AccountTransaction t: transactions){
+
+                if(t.getCategory() == c){
+                    if(t instanceof Expenditure) {
+                        amount += t.getValue();
+                    } else {
+                        amount -= t.getValue();
+                    }
+                }
+            }
+            Budget budget = budgetDAO.getBudgetByMonthOfYearAndCategory(m,y,c);
+            BudgetUpdate p = new BudgetUpdate(c.getName(), df.format((amount/budget.getBudget())*100)+ " % ");
+            report.add(p);
+        }
+
+        return report;
+    }
+
+    @FXML
+    protected void showReport(){
+        String m = String.valueOf(monthBudgetProgress.getValue());
+        int yearValue = Integer.parseInt(String.valueOf(yearBudgetProgress.getValue()));
+
+        List<AccountTransaction> transactions = transactionDAO.getAllByMonthOfYear(Month.valueOf(m),yearBudgetProgress.getValue());
+
+        double totalExpenditure = 0;
+        for (AccountTransaction transaction: transactions) {
+            if(transaction instanceof Expenditure) {
+                totalExpenditure += transaction.getValue();
+            } else {
+                totalExpenditure -= transaction.getValue();
+            }
+        }
+
+        List<Budget> budgets = budgetDAO.getAll();
+
+        double totalBudget = budgets.stream()
+                .filter(o -> o.getMonth() == Month.valueOf(m))
+                .filter(o -> o.getYear() == yearValue)
+                .mapToDouble(Budget::getBudget)
+                .sum();
+
+        double usagePercentage = calculatePercentage(totalBudget, totalExpenditure);
+
+        overallProgress.setText(df.format(usagePercentage) + " % (" + totalExpenditure + " /" + totalBudget + ")");
+
+        List<Category> categories = categoryDAO.getAll();
+
+        List<BudgetUpdate> report = this.calculateCategorizedProgress(categories, transactions,Month.valueOf(m),yearValue);
+
+        budgetReportTable.setItems(FXCollections.observableArrayList(report));
+        budgetReportTable.refresh();
+    }
+}
---
+++ expense-tracker/src/main/java/iit/asd/expensetracker/CategoryController.java
@@ -0,0 +1,193 @@
+package iit.asd.expensetracker;
+
+import iit.asd.expensetracker.entity.Category;
+import iit.asd.expensetracker.service.CategoryService;
+import iit.asd.expensetracker.service.impl.CategoryServiceImpl;
+import iit.asd.expensetracker.util.singleton.MainStage;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class CategoryController implements Initializable {
+    private boolean isUpdate = false;
+    private Category selectedCategory;
+    @FXML
+    private TextField catName;
+    @FXML
+    private TextField catAmount;
+    @FXML
+    private TableView<Category> categoryTable;
+    @FXML
+    private Button catBtnSave;
+    @FXML
+    private Button catBtnDelete;
+    @FXML
+    private Button catBtnClear;
+    private CategoryService categoryDAO;
+
+    public CategoryController() {
+        categoryDAO = new CategoryServiceImpl();
+    }
+
+    private List<Category> getAll() {
+        return categoryDAO.getAll();
+    }
+
+    private Category getById(int id) {
+        return categoryDAO.getCategoryById(id);
+    }
+
+    private void create(Category category) {
+        categoryDAO.create(category);
+    }
+
+    private void update(Category category) {
+        categoryDAO.update(category);
+    }
+
+    private void delete(int id) {
+        categoryDAO.delete(id);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initTableColumns();
+        initTableData();
+        catBtnDelete.setVisible(false);
+    }
+
+    private void initTableData() {
+        categoryTable.setItems(FXCollections.observableArrayList(this.getAll()));
+        categoryTable.refresh();
+    }
+
+    private void initTableColumns() {
+        TableColumn<Category, Integer> columnId = new TableColumn<>("ID");
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Category, String> columnName = new TableColumn<>("Name");
+        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        categoryTable.getColumns().addAll(columnId, columnName);
+    }
+
+    @FXML
+    protected void onClickBackBtnCategory() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/home-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+            Stage stage = MainStage.getInstance();
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isValidUpdate(Category category) {
+        List<Category> categories = this.getAll();
+        return catName.getText() != null &&
+                !Objects.equals(catName.getText(), "") &&
+                category != null &&
+                categories.stream().anyMatch(o -> o.getName().equals(category.getName()));
+    }
+
+    private boolean isValidCreate(Category category) {
+        List<Category> categories = this.getAll();
+        return catName.getText() != null &&
+                !Objects.equals(catName.getText(), "") &&
+                category != null &&
+                categories.stream().noneMatch(o -> o.getName().equals(category.getName()));
+    }
+
+    private boolean isValidDelete(int id) {
+        return this.getById(id) != null;
+    }
+
+    @FXML
+    protected void onSaveCategory() {
+        try {
+            if (!isUpdate) {
+                Category category = new Category(catName.getText());
+                if(isValidCreate(category)) {
+                    this.create(category);
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid category");
+                    alert.setContentText("Please enter valid category");
+                    alert.showAndWait();
+                }
+            } else {
+                selectedCategory = categoryTable.getSelectionModel().getSelectedItem();
+
+                if(isValidUpdate(selectedCategory)) {
+                    Category category = selectedCategory;
+                    category.setName(catName.getText());
+                    this.update(category);
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Budget");
+                    alert.setContentText("Please enter valid budget");
+                    alert.showAndWait();
+                }
+            }
+        } catch (NumberFormatException ne) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Budget");
+            alert.setContentText("Please enter valid budget");
+            alert.showAndWait();
+        }
+
+        clearForm();
+        initTableData();
+    }
+
+    private void clearForm() {
+        catName.clear();
+
+        catBtnSave.setText("Save");
+        catBtnDelete.setVisible(false);
+        isUpdate = false;
+    }
+
+    @FXML
+    protected void onDeleteCategory() {
+        if (isValidDelete(selectedCategory.getId())) {
+            this.delete(selectedCategory.getId());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid category");
+            alert.setContentText("Please select a valid category to proceed");
+            alert.showAndWait();
+        }
+
+        initTableData();
+        clearForm();
+    }
+
+    @FXML
+    protected void onClickSingleRow() {
+        selectedCategory = categoryTable.getSelectionModel().getSelectedItem();
+        catName.setText(selectedCategory.getName());
+
+        catBtnSave.setText("Update");
+        catBtnDelete.setVisible(true);
+        isUpdate = true;
+    }
+
+    @FXML
+    protected void clearCategoryForm() {
+        clearForm();
+    }
+}
